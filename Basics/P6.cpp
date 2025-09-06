6cb53cdac0d0a4d07477c5db9f29ee601597e275
shivamkumar <shivamkumareng7@gmail.com>
2025-09-06 20:06:24 +0530

    completed STL

--- Basics/P6.cpp
+++ Basics/P6.cpp
@@ -71,7 +71,7 @@ void explainVectors(){
 
     vec2.erase(vec2.begin()+2);             // give the exact iterator address 
 
-    vec2.erase(vec2.begin()+1, vec2.begin()+3);       // give the start and next to end element we wanna delete [)]
+    vec2.erase(vec2.begin()+1, vec2.begin()+3);       // give the start and next to end element we wanna delete [start, end)]
 
     for(auto it : vec2){
         cout<<it<<" ";
@@ -115,8 +115,252 @@ void explainVectors(){
 
 }
 
+// list
+void explainList(){
+    list<int> ls;
+    ls.push_back(1);
+    ls.emplace_back(2);
+    ls.push_front(3);
+    ls.emplace_front(4);
+
+    for(auto it : ls){
+        cout<<it<<" ";
+    }
+    cout<<endl;
+
+    // rest all functions are same as the vectors
+    // begin, end, rbegin, rend, clear, insert, size, swap
+}
+
+// deque
+void explainDeque(){
+    deque<int> dq;
+
+    //
+    dq.push_back(1);
+    dq.emplace_back(2);
+    dq.push_front(3);
+    dq.emplace_front(4);
+
+    for(auto it : dq){
+        cout<<it<<" ";
+    }
+    cout<<endl;
+
+    //
+    dq.pop_back();
+    dq.pop_front();
+
+    for(auto it : dq){
+        cout<<it<<" ";
+    }
+    cout<<endl;
+
+    cout<<dq.back()<<endl;
+    cout<<dq.front()<<endl;
+
+    // rest all functions are same as vector
+    // begin, end, rbegin, rend, clear, insert, size, swap
+}
+
+// stack
+void explainStack(){
+    stack<int> st;
+
+    st.push(1);
+    st.push(2);
+    st.push(3);
+    st.push(4);
+    st.emplace(5);
+
+    cout<<"top:"<<st.top()<<endl;
+
+    // deletes the element at the top - LIFO
+    st.pop();
+
+    cout<<"top:"<<st.top()<<endl;
+
+    cout<<"size:"<<st.size()<<endl;
+    cout<<"empty?"<<st.empty()<<endl;
+
+    stack<int> st1, st2;
+    st1.swap(st2);
+}
+
+// queue
+void explainQueue(){
+    queue<int> q;
+
+    q.push(1);
+    q.push(2);
+    q.push(3);
+    q.push(4);
+    q.emplace(5);
+
+    cout<<q.front()<<endl;
+    cout<<q.back()<<endl;
+
+    q.back() += 6;
+    cout<<q.back()<<endl;
+
+    // delete elements that came first means from front
+    q.pop();
+    cout<<q.front()<<endl;
+
+    // rest all functions are same as stack
+    // empty, size, swap
+
+}
+
+// priority queue
+void explainPQ(){
+    // max-heap
+    priority_queue<int> pq;
+
+    pq.push(7);
+    pq.push(4);
+    pq.push(10); // top max
+    pq.push(2);
+    pq.emplace(5);
+
+    cout<<pq.top()<<endl;
+    
+    pq.pop();
+    
+    cout<<pq.top()<<endl;
+
+    // rest all other functions are same
+    // size, empty, swap
+
+    //min-heap
+    priority_queue<int, vector<int>, greater<int>> pq2;
+
+    pq2.push(7);
+    pq2.push(4);
+    pq2.push(10);
+    pq2.push(2); // top min
+    pq2.push(5);
+
+    cout<<pq2.top()<<endl;
+
+}
+
+// set - sorted, unique
+void explainSet(){
+    set<int> st;
+
+    st.insert(10);
+    st.emplace(7);
+    st.insert(4);
+    st.insert(9);
+    st.insert(3);
+    st.insert(12);
+
+    set<int>::iterator it = st.find(7);              // gives the address of 7 and stores in iterator "it"
+    auto it1 = st.find(20);                          // if element doesn't exists then gives the address of st.end()
+
+    st.erase(4);
+
+    int cnt = st.count(9);                           // give the no. of occurance of the provided element. if exists then 1(unique) if not then 0
+
+    auto it2 = st.find(10);
+    st.erase(it2);
+
+    auto it3 = st.find(3);
+    auto it4 = st.find(9);
+    st.erase(it3, it4);                     // [start, end)
+
+    // rest other functions are same as above
+    // begin, end, rebegin, rend, empty, swap, size
+
+    // upper & lower bound
+    // set {9, 12}
+    auto itU = st.upper_bound(9);
+    auto itL = st.lower_bound(12);
+    cout<<*(itU)<<endl;
+    cout<<*(itL);
+}
+
+void explainMultiset(){
+    multiset<int> ms;
+
+    ms.insert(25);
+    ms.insert(15);
+    ms.insert(20);
+    ms.insert(1);
+    ms.emplace(1);
+    ms.insert(1);
+    ms.insert(1);
+    ms.emplace(20);
+    ms.emplace(20);
+    // multiset {1, 1, 1, 1, 15, 20, 20, 20, 25}
+
+    // deletes all the 20's
+    ms.erase(20);
+
+    auto it = ms.find(1);
+    ms.erase(it);                                            // erases just the first occurance of the element
+
+    ms.erase(ms.find(1), next(ms.find(1), 2));               // [start, end)
+
+    cout<<*(ms.find(1));
+
+}
+
+void explainUnorderedSet(){
+    unordered_set<int> st;
+    // same as set except it is unordered
+    // store elements in random fashoin
+    // all function of set works except upper and lower bound
+    // time complexity of all operations is mainly O(1), but rarely O(N)
+}
+
+// map - store value with unique key
+void explainMap(){
+    // initializing <key, value>
+    map<int, int> mp;
+    map<char, pair<int, int>> mp2;
+    map<pair<int,int>, int> mp3;
+
+    // storing in map
+    mp[1] = 2;                                              // {1, 2}
+    mp.emplace(2, 100);                                     // {{1, 2}, {2, 100}}
+    mp.insert({3, 150});                                    // {{1, 2}, {2, 100}, {3, 150}}
+
+    mp3[{1,7}] = 10;
+
+    // loop
+
+    for(auto it : mp){
+        cout<<it.first<<" "<<it.second<<endl;
+    }
+    cout<<"\n";
+
+    // accessing
+    cout<<mp[1]<<endl;
+    cout<<mp[5]<<endl;
+
+    auto it = mp.find(3);                           // if not found then points to .end()
+    cout<<(*it).second;
+
+    // all other functions works as above
+    // upper_bound, lower_bound, size, swap, erase, empty
+
+}
+
+void explainMultimap(){
+    // same as map but can use duplicate key
+    // can't use mp[key]
+}
+
+void unorderedMap(){
+    // same as map but stored in unsorted fashion
+    //  time complexity of all operations is mainly O(1), but rarely O(N)
+}
+
+
 int main(){
-    explainVectors();
+    explainMap();
 
     return 0;
 }
